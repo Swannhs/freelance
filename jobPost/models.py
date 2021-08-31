f77aa59ac07d757ad577ec753fcb08acde8fe98d
Swannhs <princeapurbask@gmail.com>
2021-08-31 10:39:10 +0600

    Configured small model for job post

--- jobPost/models.py
+++ jobPost/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+class Post(models.Model):
+    _id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=300)
+    slug = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def __str__(self):
+        return str(self.title)
